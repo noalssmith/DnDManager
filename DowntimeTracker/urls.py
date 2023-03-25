2a80435b26c04c17df7befa097547bea8e01f18f
Noal Smith <noalssmith@gmail.com>
2023-03-24 19:20:07 -0600

    Routing fixed

--- DowntimeTracker/urls.py
+++ DowntimeTracker/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlPatterns = [
+urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard')
 ]
--- DowntimeTracker/views.py
+++ DowntimeTracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'welcome.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
