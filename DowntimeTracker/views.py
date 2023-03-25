681f4c110bfa3ce01a7422fda2441942aa75eb03
Reagan Baxter <reagan.baxter@aggiemail.usu.edu>
2023-03-25 10:31:17 -0600

    Populating activities

--- DowntimeTracker/views.py
+++ DowntimeTracker/views.py
@@ -11,14 +11,22 @@ def index(request):
     return render(request, "welcome.html")
 
 def dashboard(request, name):
-        
-    return render(request, "dashboard.html", {"name": name})
+    current_activitys = Activity.objects.filter(player=name)
+    all_activities = Activity.objects.all()
+    if len(current_activitys) > 0:
+        current_activity = current_activitys[0]
+        percentage = int(round(current_activity.days_completed / current_activity.days, 2) * 100)
+    else:
+        current_activity = None
+        percentage = 0
+    return render(request, "dashboard.html", {"name": name, "currentActivity": current_activity, "percentage":  percentage, "allActivities":all_activities})
 
 def populate(request):
-    treasure = Activity(title="Find Treasure", description="Character spends their free time chasing the treasure of a great and feared pirate.", player="Sarah", date_started=datetime.date(2023, 3,25), days=7, days_completed=0, hidden=False)
-    treasure.save()
-    dragons = Activity(title="Dragon Hunt", description="Character follows rumors of a dragon sighting over a far village.", player="John", date_started=datetime.date(2023, 3,20), days=14, days_completed=5, hidden=False)
-    dragons.save()
-    tlc = Activity(title="Me time", description="Character spends a day pampering themselves to recover from all of their great adventures.", player="Mike", date_started=datetime.date(2023, 3,24), days=1, days_completed=0, hidden=False)
-    tlc.save()
-    return redirect('index')
+    ...
+#     treasure = Activity(title="Find Treasure", description="Character spends their free time chasing the treasure of a great and feared pirate.", player="Sarah", date_started=datetime.date(2023, 3,25), days=7, days_completed=0, hidden=False)
+#     treasure.save()
+#     dragons = Activity(title="Dragon Hunt", description="Character follows rumors of a dragon sighting over a far village.", player="John", date_started=datetime.date(2023, 3,20), days=14, days_completed=5, hidden=False)
+#     dragons.save()
+#     tlc = Activity(title="Me time", description="Character spends a day pampering themselves to recover from all of their great adventures.", player="Mike", date_started=datetime.date(2023, 3,24), days=1, days_completed=0, hidden=False)
+#     tlc.save()
+#     return redirect('index')
