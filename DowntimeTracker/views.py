3f5bcc2a7edd8350a4a10e88009128f9df4a6e22
Noal Smith <noalssmith@gmail.com>
2023-03-25 01:30:58 -0600

    Added current activity

--- DowntimeTracker/views.py
+++ DowntimeTracker/views.py
@@ -14,6 +14,7 @@ def dashboard(request, name):
     current_activity = Activity.objects.filter(player=name)
     if len(current_activity) > 0:
         current_activity = current_activity[0]
+        percentage = int(round(current_activity.days_completed / current_activity.days, 2) * 100)
     else:
         current_activity = None
-    return render(request, "dashboard.html", {"name": name, "currentActivity": current_activity},)
+    return render(request, "dashboard.html", {"name": name, "currentActivity": current_activity, "percentage": percentage},)
