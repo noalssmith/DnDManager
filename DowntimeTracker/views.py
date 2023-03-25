c7c42b5adc5476410cd33577403d90c4cb266da8
Noal Smith <noalssmith@gmail.com>
2023-03-24 20:13:44 -0600

    Added welcome page

--- DowntimeTracker/views.py
+++ DowntimeTracker/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'welcome.html')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        if name:
+            return redirect("dashboard", name=name)
+    return render(request, "welcome.html")
 
-def dashboard(request):
-    return render(request, 'dashboard.html')
+def dashboard(request, name):
+    return render(request, "dashboard.html", {"name": name})
